3b5832bec52cf171a7fa625bc1853c77860fee11
Leon Liang <leonliang_chen@icloud.com>
2018-12-30 23:32:09 +0100

    rotating mars

--- Assets/MarsController.cs
+++ Assets/MarsController.cs
@@ -8,15 +8,17 @@ public class MarsController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        if (Input.GetMouseButton(0))
-        {
-            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), -Input.GetAxis("Mouse X"), 0) * Time.deltaTime * speed);
+	void Update()
+	{
+		transform.Rotate(0f, 0.02f, 0f);
+		if (Input.GetMouseButton(0))
+		{
+			transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), -Input.GetAxis("Mouse X"), 0) * Time.deltaTime *
+			                 speed);
 
-        }
-    }
+		}
+	}
 }
