d958dea6686315a8369add6be69b6ae47ec10fc2
Leon Liang <leonliang_chen@icloud.com>
2018-12-31 11:56:38 +0100

    minor adjustments

--- Assets/MarsController.cs
+++ Assets/MarsController.cs
@@ -13,7 +13,7 @@ public class MarsController : MonoBehaviour {
 	// Update is called once per frame
 	void Update()
 	{
-		transform.Rotate(0f, 0.02f, 0f);
+		transform.Rotate(0f, -0.02f, 0f);
 		if (Input.GetMouseButton(0))
 		{
 			transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), -Input.GetAxis("Mouse X"), 0) * Time.deltaTime *
