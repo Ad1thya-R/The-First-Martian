1fc861e58f5d02eaecaf2aff9e3362946301b870
Leon Liang <leonliang@Leons-iMac.fritz.box>
2019-01-24 18:20:33 +0100

    18:20 PM

--- Assets/Scripts/Tutorial Scene/TextController.cs
+++ Assets/Scripts/Tutorial Scene/TextController.cs
@@ -10,28 +10,50 @@ public class TextController : MonoBehaviour
 
 	[SerializeField] GameObject instructionsGO;
 	[SerializeField] private Text instructions;
+
+	public static int signalStrength = 0;
+	private bool done = false;
 	
 	// Use this for initialization
 	void Start ()
 	{
 		State.instructionsArePlaying = true;
+		
+		InvokeRepeating("IncreaseSignalStrength", 0f, 0.1f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-		instructions.text = "Establishing connection to Earth...";
+		
+		instructions.text = "markwatney$ Establishing connection to Earth... \nmarkwatney$ Signal strength: " + signalStrength + "%";
 
-		if (!IsInvoking("IncreaseInstructionIndex"))
+		if (!IsInvoking("IncreaseInstructionIndex") && done)
 		{
 			InvokeRepeating("IncreaseInstructionIndex", 2f, 3f);
 		}
-		
-		instructions.text = State.instructions[instructionIndex];
+
+		if (done)
+		{
+			instructions.text = State.instructions[instructionIndex];
+		}
 
 		if (Input.GetKey(KeyCode.Return) && !State.instructionsArePlaying)
 		{
 			SceneManager.LoadScene(2);
+		}
+	}
+
+	void IncreaseSignalStrength()
+	{
+		System.Random rnd = new System.Random();
+		int increaseRate = rnd.Next(0, 4);
+		if (signalStrength + increaseRate <= 100)
+		{
+			signalStrength = signalStrength + increaseRate;
+		}
+		else
+		{
+			done = true;
 		}
 	}
 
